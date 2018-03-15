1df9ab9433370fb9a81bbe85c42230cb242f1d1a
Nat Welch <nat@natwelch.com>
2018-03-15 03:27:51 +0000

    parse incomming data

--- main.go
+++ main.go
@@ -6,11 +6,40 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 	"github.com/ifo/sanic"
 	"github.com/wcharczuk/go-chart" //exposes "chart"
 )
 
 var worker sanic.Worker
+
+// data format
+// {
+//   format: line,pie,spark
+//   data: [
+//     { x: 1, y: 2 },
+//     { pie data... label + %? }
+//     ...
+//   ]
+//   labels: {
+//     x: blah
+//     y: blab
+//   }
+// }
+type JsonData struct {
+	Format string            `json:"format"`
+	Data   []Coordinate      `json:"data"`
+	Labels map[string]string `json:"labels"`
+}
+
+type Coordinate struct {
+	X float64 `json:"x"`
+	Y float64 `json:"y"`
+}
+
+func (a *JsonData) Bind(r *http.Request) error {
+	return nil
+}
 
 func main() {
 	worker := sanic.NewWorker7()
@@ -21,8 +50,17 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.URLFormat)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
 
-	r.Get("/chart/new", func(w http.ResponseWriter, r *http.Request) {
+	r.Post("/chart/new", func(w http.ResponseWriter, r *http.Request) {
+		data := &JsonData{}
+		if err := render.Bind(r, data); err != nil {
+			render.Render(w, r, ErrInvalidRequest(err))
+			return
+		}
+
+		log.Printf("recieved: %+v", data)
+
 		graph := chart.Chart{
 			Series: []chart.Series{
 				chart.ContinuousSeries{
@@ -46,16 +84,25 @@ func main() {
 	http.ListenAndServe(":8080", r)
 }
 
-// data format
-// {
-//   format: line,pie,spark
-//   data: [
-//     { x: 1, y: 2 },
-//     { pie data... label + %? }
-//     ...
-//   ]
-//   labels: {
-//     x: blah
-//     y: blab
-//   }
-// }
+type ErrResponse struct {
+	Err            error `json:"-"` // low-level runtime error
+	HTTPStatusCode int   `json:"-"` // http response status code
+
+	StatusText string `json:"status"`          // user-level status message
+	AppCode    int64  `json:"code,omitempty"`  // application-specific error code
+	ErrorText  string `json:"error,omitempty"` // application-level error message, for debugging
+}
+
+func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	render.Status(r, e.HTTPStatusCode)
+	return nil
+}
+
+func ErrInvalidRequest(err error) render.Renderer {
+	return &ErrResponse{
+		Err:            err,
+		HTTPStatusCode: 400,
+		StatusText:     "Invalid request.",
+		ErrorText:      err.Error(),
+	}
+}
