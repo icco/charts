71de4b2e7b1e15b46878179e164ee29deb2798c6
Nat Welch <nat@natwelch.com>
2018-12-27 19:32:41 +0000

    Add server tests

--- server/server_test.go
+++ server/server_test.go
@@ -21,7 +21,21 @@ func TestGraphQLPOST(t *testing.T) {
 	h := buildGraphQLHandler()
 
 	t.Run("get a graph", func(t *testing.T) {
-		resp := doRequest(h, "POST", "/graphql", `{"query":"{ me { name } }"}`)
+		query := `
+    {"operationName":null,"variables":{},"query":"mutation {\n  createLineGraph(input: {description: \"\", data: [{x: 1.0, y: 1.0}, {x: 2.0, y: 4.0}, {x: 3.0, y: 6.0}, {x: 4.0, y: 8.0}, {x: 5.0, y: 10.0}]}) {\n    id\n  }\n}\n"}
+    `
+
+		resp := doRequest(h, "POST", "/graphql", query)
+		assert.Equal(t, http.StatusOK, resp.Code)
+		assert.Equal(t, `{"data":{"name":"test"}}`, resp.Body.String())
+	})
+
+	t.Run("create a line graph", func(t *testing.T) {
+		query := `
+    {"operationName":null,"variables":{},"query":"{\n  getGraph(id: \"bc5f70dd-d408-49cc-8949-5476b9af1e3f\") {\n    id\n    data {\n      ... on PairPoint {\n        x\n        y\n        meta {\n          key\n          value\n        }\n      }\n    }\n  }\n}\n"}
+    `
+
+		resp := doRequest(h, "POST", "/graphql", query)
 		assert.Equal(t, http.StatusOK, resp.Code)
 		assert.Equal(t, `{"data":{"name":"test"}}`, resp.Body.String())
 	})
