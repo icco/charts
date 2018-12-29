b1fc6c9e93be6c17276849d0eefc444e6953dd53
Nat Welch <nat@natwelch.com>
2018-12-29 18:58:18 +0000

    Ruby example

---
+++ examples/s&p 500 ruby/500.rb
@@ -0,0 +1,42 @@
+#! /usr/bin/env ruby
+
+require "graphql/client"
+require "graphql/client/http"
+require "json"
+require "chronic"
+
+module Chartopia
+  HTTP = GraphQL::Client::HTTP.new("https://chartopia.app/graphql") do
+    def headers(context)
+      # Optionally set any HTTP headers
+      { "User-Agent": "S&P 500 Example" }
+    end
+  end
+
+  Schema = GraphQL::Client.load_schema(HTTP)
+
+  Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+end
+
+data = JSON.parse(File.read("sp-500-data.json")).map do |r|
+  # Chartopia requires a iso8601 time format.
+  r["timestamp"] = Chronic.parse(r["timestamp"]).iso8601
+  r
+end
+
+
+rawQuery = %{
+  mutation($data: [TimePointInput]!) {
+    createTimeseriesGraph(input:{
+      description:"S&P 500 Life"
+      data: $data
+    }) {
+      id
+      url
+    }
+  }
+}
+
+CreateQuery = Chartopia::Client.parse rawQuery
+result = Chartopia::Client.query(CreateQuery, variables: {data: data})
+p result
